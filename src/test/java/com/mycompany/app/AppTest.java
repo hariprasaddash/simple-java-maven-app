ab8490d989a67c46133ff118f12bd25f938e977e
Hari Prasad Dash <hariprasaddash55@gmail.com>
2026-06-09 19:55:20 +0000

    Update test expectation

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Built by Hari using Jenkins CI Pipeline", app.getMessage());
     }
 }
